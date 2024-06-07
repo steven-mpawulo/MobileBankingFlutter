249244dcf3415c8d066270ea3119b3a8fee4685e
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-06-07 15:29:38 +0300

    creates user model

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  int id;
+  String firstName;
+  String lastName;
+  String email;
+
+  User(
+      {required this.id,
+      required this.firstName,
+      required this.lastName,
+      required this.email});
+}
