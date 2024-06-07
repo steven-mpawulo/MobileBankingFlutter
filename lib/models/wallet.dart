fbbebf94d6f49aef0aeb433e329c4639357b020a
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-06-07 21:13:57 +0300

    creates wallet model

---
+++ lib/models/wallet.dart
@@ -0,0 +1,12 @@
+class Wallet {
+  int id;
+  int accountNumber;
+  double balance;
+  int userId;
+
+  Wallet(
+      {required this.id,
+      required this.accountNumber,
+      required this.balance,
+      required this.userId});
+}
